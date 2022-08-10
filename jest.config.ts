036741d25532fbded03d6d47bfcc7b2d3c66f137
damocles217 <damm084@tutanota.com>
2022-08-10 15:29:42 -0500

    Jest config uploaded and dependencies

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+	rootDir: './',
+	verbose: true,
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	moduleFileExtensions: ['ts', 'js','json'],
+	globals: {
+		'ts-jest': {
+			babelConfig: false,
+		},
+	},
+};
+
+export default config;
